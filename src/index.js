0e197162f128142ee9a023e41de48efb1da2366e
VZaleski <70795078+VZaleski@users.noreply.github.com>
2020-10-01 13:56:52 +0300

    init: start array task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
 exports.min = function min (array) {
-  return 0;
+    if(!array || !array.length){
+        return 0;
+    }
+    let minimum = array[0];
+    for( let i = 1; i <array.length; i++){
+        if( minimum > array[i] ){
+            minimum = array[i];
+        }
+    }
+    return minimum;
 }
-
+  
 exports.max = function max (array) {
-  return 0;
-}
-
+    if(!array || !array.length){
+        return 0;
+    }
+    let maximum = array[0];
+    for( let i = 1; i <array.length; i++){
+        if( maximum < array[i] ){
+            maximum = array[i];
+        }
+    }
+    return maximum;
+  }
+  
 exports.avg = function avg (array) {
-  return 0;
-}
+    if(!array || !array.length){
+        return 0;
+    }
+    let average = array[0];
+    let sum = array[0];
+    for( let i = 1; i <array.length; i++){
+        sum += array[i];
+    }
+    average = sum / array.length;
+    return average;
+  }
+
+
